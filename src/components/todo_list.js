9add7f83da041da1db5e5fd4c84376582599c5b0
Taylor Davidson <taylordavidson@ymail.com>
2018-02-06 12:37:19 -0800

    init with dummy data set up

---
+++ src/components/todo_list.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import todoData from '../assets/data'
+
+class TodoList extends Component{
+    constructor(props){
+        super(props);
+        
+        this.state={
+            list: todoData
+        }
+    }
+
+
+    render(){
+        const listItems = this.state.list.map((v,i)=>{
+            return <li key={i} className='collection-item'>{v.title}</li>
+        })
+        return (
+            <div>
+                  <ul className="collection">{listItems}</ul>
+            </div>
+        )
+
+    }
+}
+
+export default TodoList;
